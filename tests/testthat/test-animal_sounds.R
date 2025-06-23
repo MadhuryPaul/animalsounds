2d4063d7003098240dd49ea8006b7cb83fcde930
Heather Turner <ht@heatherturner.net>
2025-06-23 15:27:51 +0100

    add unit tests

---
+++ tests/testthat/test-animal_sounds.R
@@ -0,0 +1,12 @@
+test_that("animal_sounds produces expected strings", {
+	dog_woof <- animal_sounds("dog", "woof")
+	expect_equal(dog_woof, "The dog goes woof!")
+	expect_equal(animal_sounds("cat", "miaow"), "The cat goes miaow!")
+})
+
+test_that("handles invalid inputs", {
+	expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
+				 class = "error_not_single_string")
+	expect_error(animal_sounds("cat", factor(c("miaow"))),
+				 class = "error_not_single_string")
+})
